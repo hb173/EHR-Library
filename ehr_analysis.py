553223983847e8fa084631b38c163456031d47ba
Himangshu Raj Bhantana <89568663+hb173@users.noreply.github.com>
2022-02-09 22:21:45 -0500

    Changes upon review

--- ehr_analysis.py
+++ ehr_analysis.py
@@ -9,7 +9,6 @@ Dropping 3 and N
 
 """
 
-
 def parse_data(filename: str) -> str:
     with open(filename, "r") as data:  # O(1)
         lines = []  # O(1)
@@ -18,24 +17,19 @@ def parse_data(filename: str) -> str:
             lines.append(p)  # O(1)
     return lines  # O(1)
 
-
 """
 Complexity: for fucntion num_older_than is 
-0(1)+O(1)+O(N)(O1+O2)+O(1)+O(1)+O(1)+O(1)(O2)+O(1) 
+0(1)+O(1)+O(N)(O1+O1)+O(1)+O(2)+O(1)
 we drop the constant factor
 = O(N) as our complexity of this function.
 
 """
 
 
-def num_older_than(age, lines=parse_data("PatientCorePopulatedTable.txt")):
+def num_older_than(age, filename):
     num = 0  # O(1)
-    counter = 0  # O(1)
-    for line in lines:  # O(N)
-        if counter == 0:  # O(N)
-            counter = counter + 1  # O(2)
-
-        else:  # O(1)
+    lines=parse_data(filename) #O(1)
+    for line in lines[1:]: # (N)
             age_file = datetime.datetime.now() - datetime.datetime.strptime(
                 line[2], r"%Y-%m-%d %H:%M:%S.%f"
             )  # O(1)
@@ -43,38 +37,38 @@ def num_older_than(age, lines=parse_data("PatientCorePopulatedTable.txt")):
             # print(years)
             if years > age:  # O(1)
                 num = num + 1  # O(2)
-
     return num  # O(1)
-
 
 """
 Complexity: for sick_patients is:
-= O(1)+O(1)+O(N)(O(1)+O(2)+O(1)+O(1)+O(2)+O(1)+O(1)+O(2)+(O)1)+O(1)+O(1)
-= O(2)+O(N)+O(2N)+O(N)+O(N)+O(2N)+O(N)+O(N)+O(2N)+O(N)+O(1)+O(1)
-= O(4)+O(12N)
+= O(1)+O(1)+O(N)(O(1)+O(2)+O(1)+O(1)+O(2)+O(1)+O(1)+O(2)+(O)1)+O(1)
+= O(2)+O(N)+O(2N)+O(N)+O(N)+O(2N)+O(N)+O(N)+O(2N)+O(N)+O(1)
+= O(3)+O(12N)
 We drop the constant factor
 =0(N) as our complexity of this function.
 """
 
 
-def sick_patients(lab, gt_lt, value, lines=parse_data("LabsCorePopulatedTable.txt")):
+def sick_patients(lab, gt_lt, value, filename):
+    lines=parse_data(filename) #O(1)
     output = []  # O(1)
-    counter = 0  # O(1)
-    for line in lines:  # O(N)
-        if counter == 0:  # O(1)
-            counter = counter + 1  # O(2)
-
-        else:  # O(1)
+    for line in lines[1:]: #(N)
             if gt_lt == ">":  # O(1)
                 if (line[2] == lab) and (float(line[3]) > value):  # O(2)
                     output.append(line[0])  # O(1)
             elif gt_lt == "<":  # O(1)
                 if (line[2] == lab) and (float(line[3]) < value):  # O(2)
                     output.append(line[0])  # O(1)
-    finaloutput = list(set(output))  # O(1)
+            #elif gt_lt == "=":
+                #if (line[2] == lab) and (float(line[3]) == value):
+                    #output.append(line[0])
+    if output:
+        finaloutput = list(set(output))  #O(1)
+    else:
+        finaloutput="Enter valid argument" #O(1)
     return finaloutput  # O(1)
 
 
 if __name__ == "__main__":
-    print(num_older_than(51.2))
-    print(sick_patients("METABOLIC: ALBUMIN", ">", 4.0))
+    print(num_older_than(51.2,"PatientCorePopulatedTable.txt"))
+    print(sick_patients("METABOLIC: ALBUMIN", ">", 4.0,"LabsCorePopulatedTable.txt"))
